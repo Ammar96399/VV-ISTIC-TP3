1a1d543bf6058b24ccf1b5294af4248a36bb7e83
hwang <>
2023-03-03 10:59:35 +0100

    Implementation parts for balanced str and date

--- code/tp3-balanced-strings/src/main/java/fr/istic/vv/StringUtils.java
+++ code/tp3-balanced-strings/src/main/java/fr/istic/vv/StringUtils.java
@@ -1,11 +1,26 @@
 package fr.istic.vv;
 
+import java.util.Objects;
+
 public class StringUtils {
 
     private StringUtils() {}
 
     public static boolean isBalanced(String str) {
-        return false;
+        if (Objects.nonNull(str)) {
+            if (str.equals("")) {
+                return true;
+            } else if (str.length() < 2) {
+                return false;
+            } else if (str.charAt(0) == '{' && str.charAt(str.length() - 1) == '}'
+                    || str.charAt(0) == '[' && str.charAt(str.length() - 1) == ']'
+                    || str.charAt(0) == '(' && str.charAt(str.length() - 1) == ')') {
+                return StringUtils.isBalanced(str.substring(1, str.length() - 1));
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
     }
-
 }
--- code/tp3-date/src/main/java/fr/istic/vv/Date.java
+++ code/tp3-date/src/main/java/fr/istic/vv/Date.java
@@ -2,16 +2,84 @@ package fr.istic.vv;
 
 class Date implements Comparable<Date> {
 
-    public Date(int day, int month, int year) { }
+    private int day;
+    private int month;
+    private int year;
 
-    public static boolean isValidDate(int day, int month, int year) { return false; }
+    private static int[] _daysInMonth = new int[] { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
-    public static boolean isLeapYear(int year) { return false; }
+    private static int daysInMonthOf(int month, int year) {
+        if (month == 2) {
+            if (isLeapYear(year)) {
+                return 29;
+            }
+        }
+        return _daysInMonth[month - 1];
+    }
 
-    public Date nextDate() { return null; }
+    public Date(int day, int month, int year) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
 
-    public Date previousDate() { return null; }
+    public static boolean isValidDate(int day, int month, int year) {
+        int _daysInMonth = Date.daysInMonthOf(month, year);
+        return 1 <= day && day <= _daysInMonth;
+    }
 
-    public int compareTo(Date other) { return 0; }
+    public static boolean isLeapYear(int year) { return year % 4 != 0 || year % 400 != 0 && year % 100 == 0; }
+
+    public Date nextDate() {
+        if (this.month == 12 && this.day == 31) {
+            return new Date(1, 1, this.year + 1);
+        } else {
+            int _daysInMonth = Date.daysInMonthOf(this.month, this.year);
+            if (_daysInMonth == this.day) {
+                return new Date(1, this.month + 1, this.year);
+            } else {
+                return new Date(this.day + 1, this.month, this.year);
+            }
+        }
+    }
+
+    public Date previousDate() {
+        if (this.month == 1 && this.day == 1) {
+            return new Date(31, 12, this.year - 1);
+        } else {
+            if (this.day == 1) {
+                int _lastUpperBound = Date.daysInMonthOf(this.month - 1, this.year);
+                return new Date(_lastUpperBound, this.month - 1, this.year);
+            } else {
+                return new Date(this.day - 1, this.month, this.year);
+            }
+        }
+    }
+
+    private boolean isLessThan(Date other) {
+        if (this.year < other.year) {
+            return true;
+        } else if (this.year == other.year) {
+            if (this.month < other.month) {
+                return true;
+            } else if (this.month == other.month) {
+                return this.day < other.day;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    public int compareTo(Date other) {
+        if (this.day == other.day && this.month == other.month && this.year == other.year) {
+            return 0;
+        } else if (isLessThan(other)) {
+            return -1;
+        } else {
+            return 1;
+        }
+    }
 
 }
