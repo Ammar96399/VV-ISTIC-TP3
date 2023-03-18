cce4082a45df8ea0bde0bbb5d47b3fe84413c4df
hwang <>
2023-03-18 14:37:24 +0000

    BalancedString - reimplementation and passed test

--- code/tp3-balanced-strings/src/main/java/fr/istic/vv/StringUtils.java
+++ code/tp3-balanced-strings/src/main/java/fr/istic/vv/StringUtils.java
@@ -1,5 +1,7 @@
 package fr.istic.vv;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class StringUtils {
@@ -7,17 +9,29 @@ public class StringUtils {
     private StringUtils() {}
 
     public static boolean isBalanced(String str) {
+        List<Character> stack = new ArrayList<>();
         if (Objects.nonNull(str)) {
             if (str.equals("")) {
                 return true;
             } else if (str.length() < 2) {
                 return false;
-            } else if (str.charAt(0) == '{' && str.charAt(str.length() - 1) == '}'
-                    || str.charAt(0) == '[' && str.charAt(str.length() - 1) == ']'
-                    || str.charAt(0) == '(' && str.charAt(str.length() - 1) == ')') {
-                return StringUtils.isBalanced(str.substring(1, str.length() - 1));
             } else {
-                return false;
+                for (var i = 0; i < str.length(); i++) {
+                    if (str.charAt(i) == '{' || str.charAt(i) == '[' || str.charAt(i) == '(') {
+                        stack.add(0, str.charAt(i));
+                    }
+                    if (str.charAt(i) == '}' || str.charAt(i) == ']' || str.charAt(i) == ')') {
+                        if (stack.isEmpty()) {
+                            return false;
+                        } else {
+                            char last = stack.remove(0);
+                            if (str.charAt(i) != last) {
+                                return false;
+                            }
+                        }
+                    }
+                }
+                return stack.isEmpty();
             }
         } else {
             return false;
--- code/tp3-balanced-strings/src/test/java/fr/istic/vv/StringUtilsTest.java
+++ code/tp3-balanced-strings/src/test/java/fr/istic/vv/StringUtilsTest.java
@@ -7,5 +7,31 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class StringUtilsTest {
 
+    @Test void StringContainsIllegalCharIsNotBalanced() {
+        assertFalse(StringUtils.isBalanced("1239{[]}()"));
+    }
 
+    @Test void NotEvenSizeIsNotBalanced() {
+        assertFalse(StringUtils.isBalanced("{[]}("));
+    }
+
+    @Test void NullIsNotValid() {
+        assertFalse(StringUtils.isBalanced(null));
+    }
+
+    @Test void EmptyIsBalanced() {
+        assertTrue(StringUtils.isBalanced(""));
+    }
+
+    @Test void NestedBalancedStringIsBalanced() {
+        assertFalse(StringUtils.isBalanced("[{[({})]}]"));
+    }
+
+    @Test void ComposedBalancedStringIsBalanced() {
+        assertFalse(StringUtils.isBalanced("[](){}[({})]"));
+    }
+
+    @Test void OtherCasesAreNotBalanced() {
+        assertFalse(StringUtils.isBalanced("[]([{])}"));
+    }
 }
